77f82bbcca71607d7bbd934a3e6a071c1e469626
RodneyRThomas <82400756+RodneyRThomas@users.noreply.github.com>
2021-04-13 10:15:17 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,4 +1,5 @@
 Crafty.init(500,350, document.getElementById('play-game'));
+Crafty.background('blue no-repeat');
 
 var player = Crafty.e('2D, DOM, Color, Twoway, Gravity').attr({x: 0, y: 0, w: 50, h: 50 }).color('green').gravity('Floor').twoway(200);
 var floor = Crafty.e('2D, Color, Floor').attr({x: 0, y: 500, w: 1000, h: 10 }).color('black');
